9b02e1cbf46cbeb2b2f714a7ad13289f7efac43e
19BCE0431 <67175641+19BCE0431@users.noreply.github.com>
2021-04-06 08:15:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+$(".b1").click(function(){
+    $(".b2Info").addClass("hide");
+  $(".b1Info").removeClass("hide");
+});
+
+$(".b2").click(function(){
+$(".b1Info").addClass("hide");
+  $(".b2Info").removeClass("hide");
+});
